a3dbd9b1f5a79b6102f7a7b1c4be9739469c1dc2
Lucas Gabriel <lgabriel1607@gmail.com>
2022-10-27 10:54:17 -0300

    Nevoa

--- Nevoa/Nevoa.py
+++ Nevoa/Nevoa.py
@@ -1,90 +1,143 @@
 ''' Se inscreve no tópico e manda para o servidor principal '''
-import random
-
+import random, json
+from time import sleep
 import paho.mqtt.client as mqtt
-import time
-import json
+import threading
 
-
-listaMensagem = [] #mensagens que recebo entram na lista
+listaMensagem = []  # mensagens que recebo entram na lista
 class Conexao():
-
-    #exibe as mensagens que chegam
+    # exibe as mensagens que chegam
     def retrn(self, cliente, dadosUsuario, mensagem):
         mensagemDecode = str(mensagem.payload.decode('utf-8'))
         print("Mensagem recebida", mensagemDecode)
         print('Tópico da Mensagem', mensagem.topic)
 
-    #insere as mensagens que chegam na lista
+    # insere as mensagens que chegam na lista
     def retorno(self, cliente, dadosUsuario, mensagem):
         mensagemDecode = str(mensagem.payload.decode('utf-8'))
-        listaMensagem.append(json.loads(mensagemDecode))
+        if mensagem.topic == 'nevoa/Hidrometros': #se a mensagem chegar no tópico de hidrometros, ele guarda a informação no banco
+            listaMensagem.append(json.loads(mensagemDecode))
+        #else:  se não ele tem que verificar se a mensagem que chega é pra bloquear ou não
 
-    ########### Log - Registro do cliente #################
+
+    # Log - Registro do cliente
     def retornoLog(self, cliente, dadosUsuario, nivel, buf):
         print('Log: ', buf)
 
-    ######## Verifica conexão ################
+    # Verifica conexão
     def verificaConexao(self, cliente, dadosUsuario, flags, rc):
         if rc == 0:
             print('Conectado, código de retorno= ', rc)
         else:
             print('Não foi possível se conectar, código= ', rc)
 
-    ###### metodo para publicar no tópico #######
-    #def publica(self, urlTopico, dadosEnviar):
+    # metodo para publicar no tópico
+    # def publica(self, urlTopico, dadosEnviar):
     #   return urlTopico, dadosEnviar
 
-
     def inicia(self):
-        c = True
         broker = 'broker.hivemq.com'
-        client = mqtt.Client('nevoa', random.randint(1,1000))
-        client.on_connect = self.verificaConexao #metodo do mqtt responsavel por verificar se estabeleceu a conexão ou não
+        client = mqtt.Client('nevoa', random.randint(1, 1000))
+        client.on_connect = self.verificaConexao  # metodo do mqtt responsavel por verificar se estabeleceu a conexão
+        # ou não
         client.connect(broker)
         print('Conectado no servidor')
-        client.loop_start() #para ver o retorno das chamadas
-        print('Se inscrevendo no tópico')
-        while c == True:
-            client.subscribe('nevoa/Hidrometros')
-            client.on_message = self.retorno  # inserindo a função de retorno
-            #client.on_log = self.retornoLog  # retorno do log das mensagens
-            c=False
+        return client
 
-    #para se conectar em um tópico de um hidrômetro expecifico
-    def conectaHidroexpecifico(self, matriculaConecta):
-        for matricula in listaMensagem:
-            if matriculaConecta == str(matricula['Matricula']):
-                broker = 'broker.hivemq.com'
-                client = mqtt.Client('nev')
-                client.on_connect = self.verificaConexao  # metodo do mqtt responsavel por verificar se estabeleceu a conexão ou não
-                client.connect(broker)
-                print('Conectado no servidor')
-                client.loop_start()
-                print('Se inscrevendo no tópico')
-                while True:
-                    client.subscribe(f'nevoa/Hidrometros/{matriculaConecta}')
-                    client.on_message = self.retrn  # inserindo a função de retorno
-                    # client.on_log = self.retornoLog  # retorno do log das mensagens
+    # Abre o banco
+    def abrirBanco(self, arquivo):
+        BancoAberto = []
+        with open(arquivo) as banco:
+            BancoAberto = json.load(banco)
+        return BancoAberto
+
+    # função retorna banco simplificado
+    def bancoSimplificado(self, banco):
+        listaComTodosHidrometro = []
+        for objeto in banco:
+            consumo = 0
+            vazao = 0
+            data = ''
+            bloqueado = 0
+            hidrometroMatricula = objeto['Matricula']
+            for hidrometro in banco:  # pra todos os hidrometros no banco  ele vai adicionando o valor pra mostrar uma redução de todos que tem no banco e retornar o json disso
+                if hidrometro['Matricula'] == hidrometroMatricula:
+                    consumo += hidrometro['Consumo']
+                    vazao = hidrometro['Vazao']
+                    data = hidrometro['Data']
+                    bloqueado = hidrometro['Bloqueado']
+            HidroPreenchido = {"Matricula": hidrometroMatricula, "Consumo": round(consumo, 2), "Vazao": vazao,
+                               "Data": data, 'Bloqueado': bloqueado}
+            if HidroPreenchido not in listaComTodosHidrometro:
+                listaComTodosHidrometro.append(HidroPreenchido)
+        return listaComTodosHidrometro
+
+    # Inserindo os dados no banco de dados
+    def insereBanco(self):
+        listaHidro = []
+        with open("Nevoa/BancoNevoa/bancoNevoa.json") as banco:
+            listaHidro = json.load(banco)
+        if listaMensagem:
+            for hidrometro in listaMensagem:
+                listaHidro.append(dict(hidrometro))
+                listaMensagem.remove(hidrometro)
+        with open("Nevoa/BancoNevoa/bancoNevoa.json", 'w') as arquivoBanco:
+            json.dump(listaHidro, arquivoBanco,
+                      indent=5,
+                      separators=(',', ': ')
+                      )
+
+    # se inscrevendo nos tópicos
+    def inscrevendoTopico(self):
+        client = self.inicia()
+        client.loop_start()  # para ver o retorno das chamadas
+        print('Se inscrevendo no tópico')
+        c = True
+        while c == True:
+            client.subscribe('nevoa/Hidrometros', 1)
+            client.on_message = self.retorno# inserindo a função de retorno
+            self.insereBanco()
+            sleep(10)
+            banco = self.abrirBanco("Nevoa/BancoNevoa/bancoNevoa.json")
+            listaHidro = self.bancoSimplificado(banco)
+            for hidrometro in listaHidro:
+                client.subscribe(f"nevoa/Hidrometros/{hidrometro['Matricula']}", 2)
+
+
+
+
+    # # Função para pegar os hidrometros no banco e conectar o cliente ao topico de cada hidrometro
+    # def hidroExpecifico(self):
+    #     client = self.inicia()
+    #     #client.loop_start()  # para observar o retorno das chamadas
+    #     print('Se inscrevendo no tópico')
+    #
+    #     while True:
+    #         for hidrometro in banco:
+    #
+    #
+    #         # client.on_log = self.retornoLog  # retorno do log das mensagens
+
 
 nova = Conexao()
+threading.Thread(target=nova.inscrevendoTopico).start()
+#while True:
+#    nova.hidroExpecifico()
 
+# def menu():
+#     print('''\nPor favor, selecione uma das opcções:
+#        [1] - Alterar Vazão
+#        [2] - Ver Dados
+#        [0] - Encerrar Hidrômetro''')
+#     opt = int(input('\n'))
+#     return opt
+#
+# def menuAux():
+#     nova.inicia()
+#     opt = menu()
+#     if opt == 1:
+#         mat = input('insira matricula:') #matricula vem do adm no caso
+#
+#         nova.conectaHidroexpecifico(mat)
 
-
-def menu():
-    print('''\nPor favor, selecione uma das opcções: 
-       [1] - Alterar Vazão
-       [2] - Ver Dados
-       [0] - Encerrar Hidrômetro''')
-    opt = int(input('\n'))
-    return opt
-
-def menuAux():
-    nova.inicia()
-    opt = menu()
-    if opt == 1:
-        mat = input('insira matricula:') #matricula vem do adm no caso
-
-        nova.conectaHidroexpecifico(mat)
-
-menuAux()
+# menuAux()
